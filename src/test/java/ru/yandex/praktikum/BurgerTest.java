227df576fdd2082ca740d568ca0d38943749b321
PavelSudalin <p.sudalin@infosec.ru>
2023-02-23 03:00:43 +0300

    corrected project

--- src/test/java/ru/yandex/praktikum/BurgerTest.java
+++ src/test/java/ru/yandex/praktikum/BurgerTest.java
@@ -1,14 +1,11 @@
 package ru.yandex.praktikum;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import ru.yandex.praktikum.Bun;
-import ru.yandex.praktikum.Burger;
-import ru.yandex.praktikum.Database;
-import ru.yandex.praktikum.Ingredient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,61 +16,44 @@ import static org.junit.Assert.assertEquals;
 public class BurgerTest {
     Burger burger = new Burger();
     @Mock
-    Database databaseMock;
-    List<Bun> bunMock = Arrays.asList(new Bun("grey bun",100.50F));
-    Database database = new Database();
-    List<Bun> buns = database.availableBuns();
-    List<Ingredient> ingredients = database.availableIngredients();
+    Ingredient ingredient;
+    @Mock
+    Database database;
+    private final List<Bun> buns = Arrays.asList(new Bun("grey bun",100.50F));
 
-    @Test
-    public void checkSetBunsMock() {
-        Mockito.when(databaseMock.availableBuns()).thenReturn(bunMock);
-        burger.setBuns(databaseMock.availableBuns().get(0));
-        assertEquals(databaseMock.availableBuns().get(0), burger.bun);
+    private final String bunName = "grey bun";
+    private final Ingredient ingredientSauce = new Ingredient(IngredientType.SAUCE, "hot sauce", 100F);
+    private final Ingredient ingredientFilling = new Ingredient(IngredientType.FILLING, "sausage", 300F);
+    @Before
+    public void setDefaultBun() {
+        Mockito.when(database.availableBuns()).thenReturn(buns);
     }
 
     @Test
-    public void checkAddIngredients() {
-        ingredients.forEach((n) -> burger.addIngredient(n));
-        assertEquals(ingredients, burger.ingredients);
-    }
-
-    @Test
-    public void checkRemoveIngredients() {
-        List<Ingredient> expectedRemove = Arrays.asList(ingredients.get(0));
-        burger.addIngredient(ingredients.get(0));
-        burger.addIngredient(ingredients.get(1));
-        burger.removeIngredient(1);
-        assertEquals(expectedRemove, burger.ingredients);
-    }
-
-    @Test
-    public void checkMoveIngredient() {
-        List<Ingredient> expectedMove = Arrays.asList(ingredients.get(1), ingredients.get(0));
-        burger.addIngredient(ingredients.get(0));
-        burger.addIngredient(ingredients.get(1));
-        burger.moveIngredient(0,1);
-        assertEquals(expectedMove, burger.ingredients);
+    public void checkSetBuns() {
+        burger.setBuns(database.availableBuns().get(0));
+        assertEquals(bunName, burger.bun.getName());
     }
 
     @Test
     public void checkGetPrice() {
-        burger.setBuns(buns.get(1));
-        burger.addIngredient(ingredients.get(0));
-        burger.addIngredient(ingredients.get(1));
-        assertEquals(700, burger.getPrice(), 0);
+        Mockito.when(ingredient.getPrice()).thenReturn(125.5F);
+        burger.setBuns(database.availableBuns().get(0));
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredient);
+        assertEquals(452, burger.getPrice(), 0);
     }
 
     @Test
     public void checkGetReceipt() {
-        burger.addIngredient(ingredients.get(0));
-        burger.addIngredient(ingredients.get(5));
-        burger.setBuns(buns.get(2));
-        String expected = "(==== red bun ====)" + "\n"
+        burger.setBuns(database.availableBuns().get(0));
+        burger.addIngredient(ingredientSauce);
+        burger.addIngredient(ingredientFilling);
+        String expected = "(==== grey bun ====)" + "\n"
                 + "= sauce hot sauce =" + "\n"
                 +"= filling sausage ="+ "\n"
-                + "(==== red bun ====)" + "\n\n"
-                + "Price: 1000,000000" + "\n";
+                + "(==== grey bun ====)" + "\n\n"
+                + "Price: 601,000000" + "\n";
         assertEquals(expected, burger.getReceipt());
     }
 
